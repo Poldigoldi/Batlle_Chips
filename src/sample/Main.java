fe47a9f163d6be4077d3fc1d0d7464772001349b
Leonard Goldschmidt <leo.goldschmidt@yellowdog.cot>
2019-02-05 16:52:46 +0000

    I changed the title to

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Hello this is my first branch by Leo");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
